7266db5ffc05a6882f66b8c27057a0a06cf090ed
Jawad Muhammadi <jawad10@Jawads-MacBook-Pro.local>
2018-10-16 11:55:18 -0700

    Installed package.json and set up server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,11 @@
+var express = require('express');
+var app = express();
+//app.use(bodyParser.json());
+var path = require('path');
 
+app.get('/', function(req, res) {
+  res.send('hello world from express');
+});
+app.use(express.static('client'));
 
+app.listen(3000, () => {console.log('listening on port 3000')});
