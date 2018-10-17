9f2063ad0bccbcdf14ceb71d5ddb5003db840672
Jawad Muhammadi <jawad10@Jawads-MacBook-Pro.local>
2018-10-16 19:01:14 -0700

    submitted data to server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,23 +1,25 @@
 
 const express = require('express');
 const path = require('path');
-const parser = require('body-parser');
-const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+const Parser = require('./parser.js');
+const parser = new Parser();
+const fs = require('fs');
 
-app = express();
-app.use(express.static('./client'));
-
-app.use(parser.json());
-app.use(morgan('dev'));
-
-port = 3000;
-app.listen(port);
-console.log(`CSV istening on port ${port}...`);
-
-app.get('/', (req, res) => {
-  res.render(path.join(__dirname, './client/index.html'));
-});
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, './client')));
 
 app.post('/upload_json', (req, res) => {
-	console.log(req.body)
-})
+	var json = JSON.parse(req.body.text);
+	res.set('Content-Type', 'text/plain');
+	res.end(parser.cvsCreator(json));
+});
+app.listen(3000, () => {
+	console.log("listening on port 3000");
+});
+
+
+
+
