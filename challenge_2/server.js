028d4d13d21d7c9cb2c974f2c364f4db221dc30f
Jawad Muhammadi <jawad10@Jawads-MacBook-Pro.local>
2018-10-16 19:44:05 -0700

    added multer for file upload

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -6,15 +6,18 @@ const app = express();
 const Parser = require('./parser.js');
 const parser = new Parser();
 const fs = require('fs');
+const multer = require('multer');
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, './client')));
+app.use(multer().single('text'));
 
 app.post('/upload_json', (req, res) => {
-	var json = JSON.parse(req.body.text);
+	console.log(req.file.buffer.toString());
+	var json = JSON.parse(req.file.buffer.toString());
 	res.set('Content-Type', 'text/plain');
-	res.end(parser.cvsCreator(json));
+	res.send(parser.cvsCreator(json));
 });
 app.listen(3000, () => {
 	console.log("listening on port 3000");
