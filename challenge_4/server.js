adad692e79f4ed5f36ef985471d5652b0b471afd
Jawad Muhammadi <jawad10@Jawads-MacBook-Pro.local>
2018-10-18 11:36:15 -0700

    set up server

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
 
 
+app.get('/', function(req, res) {
+	res.send('Hello there');
+});
+
+app.listen(port, () => {
+	console.log('listening to port 3000');
+})
