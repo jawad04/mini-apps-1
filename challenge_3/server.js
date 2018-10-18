99189eb4efabc6a207d5be1cb6f8d40041c86640
Jawad Muhammadi <jawad10@Jawads-MacBook-Pro.local>
2018-10-17 17:37:27 -0700

    started to connect to mysql database

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -3,12 +3,54 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
+
+const mysql = require('mysql');
+
 //app.use(express.static('public')
 //app.use(express.json());
 app.use(express.static(path.join(__dirname, './public')));
 // app.get('/', function(req, res) {
 // 	res.send('Get request is working');
 // });
+
+/*************** ADD DATABASE *************/
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'jawad',
+	password: 'jawad123',
+	database: 'checkout'
+});
+
+var getAll = function(callback) {
+  connection.query('SELECT * FROM checkout', function(error, results, fields) {
+  	if (error) {
+  	  callback(error, null);
+  	} else {
+  	  callback(null, results);
+  	}
+  });
+}
+
+var insertOne = function(count, name, email, password, line1, line2, city, state, zipCode, phone, creditCard, expDate, CVV, billingZip, callback) {
+  const query = `INSERT INTO checkout (count, name, email, password, line1, line2, city, state, zipCode, phone, creditCard, expDate, CVV, billingZip) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+  connection.query(query,[count, name, email, password, line1, line2, city, state, zipCode, phone, creditCard, expDate, CVV, billingZip, callback], function(error, result, fields) {
+  	callback(error, null);
+  });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(port, () => {
 	console.log('Listening to port 3000');
 });
