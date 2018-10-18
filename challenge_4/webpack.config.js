8585363b006d183d3a3b2bc4c0960f72c3c8bd00
Jawad Muhammadi <jawad10@Jawads-MacBook-Pro.local>
2018-10-18 10:50:31 -0700

    configuration done

---
+++ challenge_4/webpack.config.js
@@ -0,0 +1,24 @@
+// 
+// var path = require('path');
+// var webpack = require('webpack');
+module.exports = {
+      entry: __dirname + '/client/src/index.jsx',
+      module: {
+        rules: [
+          { 
+            test: [/\.jsx$/],
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-react', '@babel/preset-env']
+              }
+            }
+          }
+        ]
+      },
+       output: {
+        filename: 'bundle.js',
+        path: __dirname + '/client/dist'
+      }
+    };
